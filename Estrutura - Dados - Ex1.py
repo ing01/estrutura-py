5165bd33be477b69e870420491ae811782197b02
Ingrid <71271942+ing01@users.noreply.github.com>
2021-04-26 10:07:14 -0300

    Create Estrutura - Dados - Ex1.py

---
+++ Estrutura - Dados - Ex1.py
@@ -0,0 +1,16 @@
+# Elabore uma estrutura para representar um produto (código, nome, preço). Aplique 10% de aumento no preço do produto e apresente.
+
+class produto:
+  codigo = 0
+  nome = ''
+  preco = 0
+
+def main():
+  prod = produto()
+  prod.codigo = int(input('Insira o código do produto: '))
+  prod.nome = input('Inira o nome do produto: ')
+  prod.preco = float(input('Insira o preço do produto: '))
+  prod.preco = prod.preco + (prod.preco * 10 / 100)
+  print('Código: ',prod.codigo,'\n','Nome: ',prod.nome,'\n', F'Preço: R${prod.preco:.2f}')
+
+main()
